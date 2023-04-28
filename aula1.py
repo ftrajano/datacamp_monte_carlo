84f762349275421219c9c3f493e4d6c68aafa699
ftrajano <ftrajano@gmail.com>
2023-04-28 15:13:09 -0300

    Adding numpy docstring

--- aula1.py
+++ aula1.py
@@ -1,17 +1,27 @@
 import numpy as np
 import random
-import sys
 
-def roll_dice(n,seed):
+def roll_dice(n: int, seed: int) -> str:
+    """
+    Simulates rolling a six-sided dice n times using a given random seed.
+
+    Parameters
+    ----------
+    n : int
+        Number of times to roll the dice.
+    seed : int
+        Seed for the random number generator.
+
+    Returns
+    -------
+    str
+        A string containing the mean outcome of the dice rolls, formatted as
+        "A média dos lançamentos é X", where X is the mean outcome rounded to
+        two decimal places.
+    """
     random.seed(seed)
     dice_rolls = []
     for i in range(n):
-        dice_rolls.append(random.randint(1,6))
+        dice_rolls.append(random.randint(1, 6))
     mean_outcome = np.mean(dice_rolls)
-    return f"A média dos lançamentos é {mean_outcome}"
-
-
-if __name__ == '__main__':
-    n = int(sys.argv[1])
-    seed = int(sys.argv[2])
-    print(roll_dice(n,seed))
+    return f"A média dos lançamentos é {mean_outcome:.2f}"
